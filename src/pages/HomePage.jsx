b272e5aa02ab706f762b4fe99bd6237709886819
Manvanth <manvanthgolyady03@gmail.com>
2026-06-18 17:15:53 +0530

    used data from the backend to generate the HTML

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
+import { useEffect,useState } from "react";
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
-  axios.get('http://localhost:3000/api/products').then((response) =>{
-        console.log(response.data)
-  })
+  let [products, setProducts]=useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data)
+    });
+  },[]);
 
   return (
     <>
@@ -19,10 +23,7 @@ export function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -32,8 +33,11 @@ export function HomePage() {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
-                  <div className="product-rating-count link-primary">{product.rating.count}</div>
+                    src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+                  />
+                  <div className="product-rating-count link-primary">
+                    {product.rating.count}
+                  </div>
                 </div>
 
                 <div className="product-price">Rs. {product.priceCents}</div>
