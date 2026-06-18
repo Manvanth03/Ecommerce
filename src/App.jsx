924cba8c8a22321d7d7c63c706d3f8bc0b3afb56
Manvanth <manvanthgolyady03@gmail.com>
2026-06-18 17:47:19 +0530

    Generate HTML for cart item

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   let [cart,setCart]=useState([])
 
 useEffect(()=>{
-      axios.get('/api/cart-items').then((response)=>{
+      axios.get('/api/cart-items?expand=product').then((response)=>{
         setCart(response.data)
     })
   },[]);
